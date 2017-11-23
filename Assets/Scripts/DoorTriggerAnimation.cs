d55115bbbe2786cd0a1d3565a34822208b3bea55
Milla Havanka <milla.havanka@mekaselska.fi>
2017-11-23 19:14:51 +0200

    Project snapshot

---
+++ Assets/Scripts/DoorTriggerAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTriggerAnimation : MonoBehaviour {
+
+	Animator dooranim;
+	public Transform door;
+	public float direction;
+
+	void Awake () {
+		dooranim = door.GetComponent<Animator>();
+	}
+
+
+
+
+	void OnTriggerEnter (Collider col) {
+		dooranim.SetFloat("direction", 1);
+		dooranim.SetTrigger("OpenDoorTrigger");
+
+	}
+
+	void OnTriggerExit (Collider col) {
+		dooranim.SetFloat("direction", -1);
+		dooranim.SetTrigger("CloseDoorTrigger");
+
+	}
+		
+}
---
+++ Assets/Scripts/MoveZombie.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveZombie : MonoBehaviour {
+
+	public Transform player;
+	//Quaternion z_rotation;
+	float speed = 2.0f;
+
+	void Update()
+	{
+		Vector3 targetDir = player.position - transform.position;
+		targetDir.y = 0;
+		float step = speed * Time.deltaTime;
+		Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+		Debug.DrawRay(transform.position, newDir, Color.red);
+		transform.rotation = Quaternion.LookRotation(newDir);
+
+	}
+}
